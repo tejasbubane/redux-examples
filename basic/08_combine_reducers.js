e96ae5c91d3f1533a3ad824bff6b7054c56f2090
Tejas Bubane <tejasbubane@gmail.com>
2017-04-23 15:27:41 +0530

    Combine ADD_TODO and TOGGLE_TODO reducers into a single reducer

---
+++ basic/08_combine_reducers.js
@@ -0,0 +1,87 @@
+// Simply combining reducers the previous two reducers ADD_TODO and TOGGLE_TODO
+// Working example: https://jsfiddle.net/tejasbubane/wehnv853/
+
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    case "TOGGLE_TODO":
+      return state.map(todo => {
+        if(todo.id === action.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
+
+const testAddTodo = () => {
+  let stateBefore = [],
+      action = {
+        type: "ADD_TODO",
+        id: 1,
+        text: "Get some bread"
+      },
+      stateAfter = [
+        {
+          id: 1,
+          text: "Get some bread",
+          completed: false
+        }
+      ];
+  Object.freeze(stateBefore);
+  Object.freeze(action);
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+  let stateBefore = [
+    {
+      id: 1,
+      text: "Get some bread",
+      completed: false
+    },
+    {
+      id: 2,
+      text: "Get some butter",
+      completed: false
+    }
+  ];
+  let action = {
+    type: "TOGGLE_TODO",
+    id: 2
+  };
+  let stateAfter = [
+    {
+      id: 1,
+      text: "Get some bread",
+      completed: false
+    },
+    {
+      id: 2,
+      text: "Get some butter",
+      completed: true
+    }
+  ]
+
+  Object.freeze(stateBefore);
+  Object.freeze(stateAfter);
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+testAddTodo();
+testToggleTodo();
+console.log("All tests passed!");
