c987ae9b69cb9661950831db06e22aacd6c5ada0
Tejas Bubane <tejasbubane@gmail.com>
2017-05-06 16:05:51 +0530

    Setup a separate todos app

---
+++ todoApp/components/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoApp = () => (
+  <h1>TODOs</h1>
+);
+
+export default TodoApp;
---
+++ todoApp/webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+
+  devtool: 'source-map',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    modules: ["components", "node_modules"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+
+  plugins: [
+    // cleaning up the destination directory before writing new files
+    new CleanWebpackPlugin(['dist']),
+
+    new HtmlWebpackPlugin({
+      title: 'TodoApp',
+      template: 'index_template.ejs'
+    })
+  ]
+}
